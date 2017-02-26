04e3c3a14567c51c8d38ff1fd3f4104eea061f8b
fweidemann14 <filip.weidemann@outlook.de>
2017-02-26 02:59:07 +0100

    Implemented a walkie talkie

--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		SimpleServer myServer = new SimpleServer();
-		while(true){
-			myServer.writeSocketContent();
-			//Git check
+		Scanner myScanner;
+		String write;
+		while(true) {
+			System.out.println("Wanna send some data man?\n");
+			
+			write = IO.readString();
+			if(write.equals("True")) {
+				myServer.writeSocketContent("Moin");
+			} else if (write.equals("False")) {
+				myServer.readSocketContent();
+			} else {
+				System.out.println("Only True or False accepted.");
+			}
+			
 		}
 		
 	}
--- SimpleServer.java
+++ SimpleServer.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,7 +13,8 @@ public class SimpleServer{
 	private int port;
 	private ServerSocket serverSocket;
 	private Socket clientSocket;
-	private String output;
+	private PrintWriter out;
+	private BufferedReader in;
 	
 	public SimpleServer() throws IOException{
 		this.port = 1337;
@@ -21,12 +25,22 @@ public class SimpleServer{
 	public SimpleServer(int port) throws IOException{
 		this.port = port;
 		serverSocket = new ServerSocket(port);
-		clientSocket = serverSocket.accept();
 	}
 	
-	public String writeSocketContent() throws IOException{
-		return clientSocket.getOutputStream().toString();
+	public void writeSocketContent(String msg) throws IOException{
+		out = new PrintWriter(clientSocket.getOutputStream(), true);
+		out.println(msg);
 	}
-	
+
+	public void readSocketContent() throws IOException {
+		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		String line;
+		
+		while ((line = in.readLine()).length() != 0) {
+		    System.out.println(line);
+		}
+		
+	}	
 	
 }
